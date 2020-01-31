c759801fe25653e5f3bf7ecf2a3d0d22b59f5f27
Ricardo Fiel <rifiel@microsoft.com>
2020-01-31 14:20:42 +0000

    Stable default namespace

--- republish.sh
+++ republish.sh
@@ -1,2 +1,3 @@
-kubectl delete -f deploy/deploy-publisher-job.yaml
+kubectl apply -f deploy/deploy-consumer.yaml
+kubectl delete -f deploy/deploy-publisher-job.yaml --ignore-not-found
 kubectl apply -f deploy/deploy-publisher-job.yaml
